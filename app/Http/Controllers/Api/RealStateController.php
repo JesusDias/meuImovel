ffd91bb39ed3f4112b1d651c9c6b70ea2c186d4e
JesusDias <jesussouzadias18@gmail.com>
2020-04-06 12:44:20 -0300

    Metodo update e delete

--- app/Http/Controllers/Api/RealStateController.php
+++ app/Http/Controllers/Api/RealStateController.php
@@ -23,6 +23,28 @@ class RealStateController extends Controller
         return response()->json($realState, 200);
     }
 
+    //============LISTAGEM COM FILTRO=========================
+    public function show($id)
+    {   
+       
+        try{
+            //busca o imóvel que tenha esse id
+            $realState = $this->realState->findOrFail($id);
+        
+            //retorna esse imovel que tenah o id informado com o statusCode
+        return response()->json([
+            'data' => [
+                'msg' => $realState
+            ]
+        ], 200);
+
+        } catch (\Exception $e) {
+            //sinão eu retorno essa mensagem de erro
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+        
+    }
+
     //==============SALVAR====================================
     public function store(Request $request)
     {   
@@ -51,10 +73,11 @@ class RealStateController extends Controller
         //variavel recebendo tudo que foi enviado
         $data = $request->all();
         try{
-            //salvo o que foi digitado
+            //salva nessa variável o imovel que tenha esse id
             $realState = $this->realState->findOrFail($id);
+            //chama o metodo update pra esses novos dados que estão na variável $data
             $realState->update($data);
-            //retorno o que foi editado
+            //retorno a mensagem de atualizado e o StatusCode
         return response()->json([
             'data' => [
                 'msg' => 'Imóvel atualizado com sucesso!'
@@ -67,4 +90,27 @@ class RealStateController extends Controller
         }
         
     }
+
+     //======================DELETAR===================================
+     public function destroy($id)
+     {   
+         try{
+             //busca esse imovel que tenha esse id
+             $realState = $this->realState->findOrFail($id);
+             //deleta o imovel que tenha esse id
+             $realState->delete($id);
+
+             //retorno a mensagemde imovel removido e statusCode 
+         return response()->json([
+             'data' => [
+                 'msg' => 'Imóvel removido com sucesso!'
+             ]
+         ], 200);
+ 
+         } catch (\Exception $e) {
+             //sinão, eu retorno essa mensagem de erro com StatusCode
+             return response()->json(['error' => $e->getMessage()], 401);
+         }
+         
+     }
 }
